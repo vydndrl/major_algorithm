a4795a4dd0fb4c606ffb4efe19ca204a0d6692c7
vydndrl <vydndrl1234@naver.com>
2024-06-24 23:48:18 +0900

    0624

--- src/A2dfs/D4피로도.java
+++ src/A2dfs/D4피로도.java
@@ -4,6 +4,7 @@ package A2dfs;
 public class D4피로도 {
 //    순열
     static boolean[] visited;
+    static int answer;
     public static void main(String[] args) {
         int k = 80;
         int[][] dungeons = {{80,20},{50,40},{30,10}};
@@ -12,18 +13,22 @@ public class D4피로도 {
         System.out.println("solution = " + solution);
     }
 
-    private static int solution(int k, int[][] dungeons) {
+    public static int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
 
-        dfs(k, dungeons, visited);
+        dfs(0, k, dungeons);
+
+        return answer;
     }
 
-    private static void dfs(int k, int[][] dungeons, boolean[] visited) {
+    public static void dfs(int start, int k, int[][] dungeons) {
         for (int i = 0; i < dungeons.length; i++) {
-            if (dungeons[i][0] <= k) {
+            if (!visited[i] && dungeons[i][0] <= k) {
                 visited[i] = true;
-                dfs(i, dungeons, visited);
+                dfs(start + 1, k - dungeons[i][1], dungeons);
+                visited[i] = false;
             }
         }
+        answer = Math.max(answer, start);
     }
 }
